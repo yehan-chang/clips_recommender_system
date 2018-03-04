5205041d41bb428cc1482ee29225ee57adf24338
yehan <yehan.cyh@gmail.com>
2018-03-04 11:09:06 +0800

    test commit

--- clips/app.py
+++ clips/app.py
@@ -10,6 +10,7 @@ from display_result import processFinal
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
+# test commit
 async_mode = None
 
 app = Flask(__name__)
